0888403e0b7038fb5a5d12543967e446b0f958fd
savrg <127246959+savrg@users.noreply.github.com>
2023-03-07 15:07:58 -0500

    Update MA493_Proj1Q1b.m

--- MA493_Proj1Q1b.m
+++ MA493_Proj1Q1b.m
@@ -3,70 +3,66 @@
 clear all
 close all
 load Q1data.mat;
-% 
-% Set the number of data vectors (n) and the dimension of the data space (m)
+
+function [c,IndexSet]= kppinitial(XData,k,varargin)
+
 [n,m] = size(XData);
 
-% Set the number of clusters (k)
-k=5;
-
-% Assign each data vector, randomly to one of the k clusters
 IndexSet = randi(k,n,1);
 
-% Plot the data
-scatter(XData(:,1),XData(:,2),64,IndexSet,'filled');
-hold on
-
-% Create data structures to store the weight vectors for cluster (c), and the 
-% weight vectors from the previous iteration (cPrev)
 c = zeros(k,m);
-cPrev = zeros(k,m);
 
-BAD [idx,c] = kmeans(XData,k);
+if nargin == 2
+    randIndex = randi(n);
 
-%% The Alternating Minimization Scheme
-doneFlag=0;
+    c(1,:) = XData(randIndex,:);
+else
+    init = varargin{1};
+    c(1,:) = XData(init,:);
+end
 
-% Keep alternating updates to weight vectors and cluster assignments until weight 
-% vectors no longer change their locations
+closestCluster=zeros(n,1);
 
-while (~doneFlag)
+for l = 2:k 
+    for d=1:n
 
-    % Update the weight vectors in each cluster via the centroid formula
-    for i=1:k 
+        % Store the coordinates of the current data vector
+        xD = XData(d,:);
 
-        % Find the indices for all data vectors currently in cluster i
-        ClusterIndices = find(IndexSet==i);
+        % Set the minimum distance tracker to be a very large number
+        sqDistMin=1e16;
 
-        % Find the number of data vectors currently in cluster i
-        NumVecsInCluster = size(ClusterIndices,1);
+        % Find the closest weight vector (cluster) to the current data
+        % vector
+        for i=1:l-1
+            sqDist = norm(c(i,:)-xD,2);
 
-        % Create a data structure to store weight vector for the current
-        % cluster
-        c(i,:)=0; 
-
-        % Update cluster vector using the centroid formula
-        for j=1:NumVecsInCluster
-            for l=1:m
-                c(i,l) = c(i,l) + XData(ClusterIndices(j,1),l)/NumVecsInCluster;
+            % If the distance is less than the current min, assign the
+            % current data vector to this cluster
+            if sqDist<sqDistMin
+                closestCluster(d)=i;
+                sqDistMin=sqDist;
             end
-        end
 
+        end
     end
 
-    % Plot the updated weight vectors for each cluster
-    scatter(XData(:,1),XData(:,2),64,IndexSet,'filled')
-    hold on
-    scatter(c(:,1),c(:,2),200,linspace(1,k,k),'filled')
-    pause
-    
-    %% Now reassign all data vectors to the closest weight vector (cluster)
+     % Update the assignments of the data vectors to their new clusters
+    IndexSet = closestCluster;
 
-    % Create a data structure to store closest weight vector for each data
-    % point
-    closestCluster=zeros(n,1);
+    DistClust = zeros(l-1,1+m);
 
-    % Reassign each data vector to the new, closest cluster
+    for y = 1:l-1
+        PntClosest = XData(IndexSet == y,:);
+
+        [MaxDistFromClosestCluster,IndexClosest] = max(sum(PntClosest - c(y,:)).^2,2);
+        DistClust(y,:) = [MaxDistFromclosestCluster,PntClosest(IndexClosest,:)];
+    end
+
+    [~, NextCentroid] = max(DistClust(:,1));
+    c(l,:) = DistClust(NextCentroid,2:end);
+end
+
     for d=1:n
 
         % Store the coordinates of the current data vector
@@ -92,20 +88,4 @@ while (~doneFlag)
 
     % Update the assignments of the data vectors to their new clusters
     IndexSet = closestCluster;
-    
-    % Plot the data and the updated weight vectors
-    hold off
-    scatter(XData(:,1),XData(:,2),64,IndexSet,'filled')
-    hold on
-    scatter(c(:,1),c(:,2),200,linspace(1,k,k),'filled')
-    hold off
-    pause
-    
-    % Terminate the alternating scheme if the weight vectors are unaltered
-    % relative to the previous iteration
-    if c==cPrev
-        doneFlag=1;
-    else
-        cPrev=c;
-    end
 end
